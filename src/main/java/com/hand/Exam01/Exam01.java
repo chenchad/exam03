f4931a336f026d964abd68ad051b7827c8b92977
chenwenfeng <563266443@qq.com>
2015-08-10 17:27:15 +0800

    exam01

---
+++ src/main/java/com/hand/Exam01/Exam01.java
@@ -0,0 +1,52 @@
+package com.hand.Exam01;
+
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class Exam01 {
+
+	public static void main(String[] args) {
+		new Get().start();
+
+	}
+
+}
+class Get extends Thread{
+	
+	HttpClient client=HttpClients.createDefault();
+	
+	public void run(){
+		HttpGet get=new HttpGet("http://www.manning.com/gsmith/SampleChapter1.pdf");
+		try {
+			
+			
+			
+			
+			
+			HttpResponse response = client.execute(get);
+			HttpEntity entity= response.getEntity();
+			String result=EntityUtils.toString(entity,"UTF-8");
+			
+			System.out.println(result);
+			
+			
+			
+			
+			
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
