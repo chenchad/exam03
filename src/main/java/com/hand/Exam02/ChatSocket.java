0607d1043f50e2c6e99c57a4d0b7bdbf357cfd02
chenwenfeng <563266443@qq.com>
2015-08-10 17:49:57 +0800

    exam2

---
+++ src/main/java/com/hand/Exam02/ChatSocket.java
@@ -0,0 +1,48 @@
+package com.hand.Exam02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.Socket;
+
+public class ChatSocket extends Thread {
+   Socket socket;
+	
+	public ChatSocket(Socket s){
+	   this.socket=s;
+   }
+	 
+	public void out(String out){
+		try {
+			socket.getOutputStream().write(out.getBytes("UTF-8"));
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void run(){
+	    try {
+			BufferedReader br=new BufferedReader(
+					new InputStreamReader(
+							socket.getInputStream(),"UTF-8"));
+			String line=null;
+			while ((line=br.readLine())!=null) {  
+			   ChatManager.getChatManager().pubilsh(this, line);  
+			}
+			br.close();
+			
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+}
---
+++ src/main/java/com/hand/Exam02/MySeverSocket.java
@@ -0,0 +1,11 @@
+package com.hand.Exam02;
+
+
+public class MySeverSocket {
+
+
+	public static void main(String[] args) {
+         new SeverListener().start();
+	}
+
+}
